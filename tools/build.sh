c22db19eb3e2d0b73b7c7638f8f055d03cb3d41e
Ivan Naranjo <ivannaranjo@users.noreply.github.com>
2016-11-18 15:23:15 +0000

    Creating cloud build (#17)
    * Adding new cloud build definition file.
    
    * Moving the file to the better location.
    
    * No need for the tools, using gcloud beta container build.
    
    * Building the image locally.
    
    * Adding scripts and templatizing the build.
    
    * Check for errors, check for the existence of the build definition file.

---
+++ tools/build.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+# This script will build the ASP.NET Core image.
+#   $1, the image to build.
+#   $2, repository for the resulting image.
+
+# Exit on error or undefined variable
+set -eu
+
+if [ -z "$1" ]; then
+    echo "Must specify the directory of the image to build."
+    echo 1
+fi
+if [ ! -f "$1/cloudbuild.yaml.inc" ]; then
+    echo "The file $1/cloudbuild.yaml.inc does not exist."
+    exit 1
+fi
+
+export REPO=$2
+if [ -z "$REPO" ]; then
+    echo "Must specify the name of the repository."
+    exit 1
+fi
+
+readonly stage_dir=$(mktemp -d build.XXXXX)
+
+# Process the template.
+envsubst < "$1/cloudbuild.yaml.inc" > "${stage_dir}/cloudbuild.yaml"
+
+# Start the build.
+gcloud beta container builds submit "$1"  --config="${stage_dir}/cloudbuild.yaml"
+
+# Cleanup.
+rm -rf "${stage_dir}"
+
