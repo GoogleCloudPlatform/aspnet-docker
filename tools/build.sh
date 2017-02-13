cccc2685acb9d660f1575f63a6ac057a59782734
Ivan Naranjo <ivann@google.com>
2017-02-13 14:14:08 +0000

    Renaming directory, refactoring build for runtime.

--- tools/build.sh
+++ tools/build.sh
@@ -6,40 +6,34 @@
 # Exit on error or undefined variable
 set -eu
 
-export readonly STAGE_DIR=$(mktemp -d .build.XXXXX)
+readonly stage_dir=$(mktemp -d .build.XXXXX)
 
 trap cleanup 0 1 2 3 13 15 # EXIT HUP INT QUIT PIPE TERM
 
 cleanup() {
-	rm -rf "$STAGE_DIR"
+	rm -rf "${stage_dir}"
 }
 
-readonly image_name="aspnet"
-readonly tag="1.0.3"
+readonly image_dir="$1"
 
-export IMAGE_DIR="$1"
-
-if [ -z "$IMAGE_DIR" ]; then
+if [ -z "${image_dir}" ]; then
     echo "Must specify the directory of the image to build."
     echo 1
 fi
-if [ ! -f "$IMAGE_DIR/cloudbuild.yaml.in" ]; then
-    echo "The file $IMAGE_DIR/cloudbuild.yaml.in does not exist."
+if [ ! -f "${image_dir}/cloudbuild.yaml.in" ]; then
+    echo "The file ${image_dir}/cloudbuild.yaml.in does not exist."
     exit 1
 fi
 
-readonly repo=$2
-if [ -z "$repo" ]; then
+export readonly REPO=$2
+if [ -z "${REPO}" ]; then
     echo "Must specify the name of the repository."
     exit 1
 fi
 
-export IMAGE="${repo}/${image_name}:${tag}"
-
-
 # Process the template.
-envsubst < "${IMAGE_DIR}/cloudbuild.yaml.in" > "${STAGE_DIR}/cloudbuild.yaml"
+envsubst < "${image_dir}/cloudbuild.yaml.in" > "${stage_dir}/cloudbuild.yaml"
 
 # Start the build.
 # Use alpha build command since some jenkins machines are on old versions of gcloud.
-gcloud alpha container builds create "$IMAGE_DIR" --config="${STAGE_DIR}/cloudbuild.yaml"
+gcloud alpha container builds create "${image_dir}" --config="${stage_dir}/cloudbuild.yaml"
