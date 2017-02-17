f041b515b585655bfb940b14d1fdcbb1acb27039
Ivan Naranjo <ivann@google.com>
2017-02-17 14:01:20 +0000

    Refactoring of repo, adding common script to build, updated base image.

--- tools/build.sh
+++ tools/build.sh
@@ -6,34 +6,36 @@
 # Exit on error or undefined variable
 set -eu
 
+if [ -z "${1:-}" ]; then
+    echo "Must specify the image directory."
+    exit 1
+fi
+
+if [ -z "${2:-}" ]; then
+    echo "Must specify the name of the repo."
+    exit 1
+fi
+
+readonly cloudbuild_template="$1/cloudbuild.yaml.in"
+if [ ! -f "${cloudbuild_template}" ]; then
+    echo "The file ${cloudbuild_template} does not exist."
+    exit 1
+fi
+
+# Stage dir for the build.
 readonly stage_dir=$(mktemp -d .build.XXXXX)
+readonly cloudbuild_expanded="${stage_dir}/cloudbuild.yaml"
 
+# Ensure cleanup of the stage dir.
 trap cleanup 0 1 2 3 13 15 # EXIT HUP INT QUIT PIPE TERM
-
 cleanup() {
 	rm -rf "${stage_dir}"
 }
 
-readonly image_dir="$1"
-
-if [ -z "${image_dir}" ]; then
-    echo "Must specify the directory of the image to build."
-    echo 1
-fi
-if [ ! -f "${image_dir}/cloudbuild.yaml.in" ]; then
-    echo "The file ${image_dir}/cloudbuild.yaml.in does not exist."
-    exit 1
-fi
-
-export readonly REPO=$2
-if [ -z "${REPO}" ]; then
-    echo "Must specify the name of the repository."
-    exit 1
-fi
-
 # Process the template.
-envsubst < "${image_dir}/cloudbuild.yaml.in" > "${stage_dir}/cloudbuild.yaml"
+export readonly VERSION=$(date +"%Y-%m-%d_%H_%M")
+export readonly REPO=$2
+envsubst < "${cloudbuild_template}" > "${cloudbuild_expanded}"
 
 # Start the build.
-# Use alpha build command since some jenkins machines are on old versions of gcloud.
-gcloud alpha container builds create "${image_dir}" --config="${stage_dir}/cloudbuild.yaml"
+gcloud alpha container builds create "$1" --config="${cloudbuild_expanded}"
