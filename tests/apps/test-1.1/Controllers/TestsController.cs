69f7e0e4956114216c75bd6a5341c58e3c12cf94
Ivan Naranjo <ivann@google.com>
2017-07-25 14:08:18 +0100

    Adding test for .NET Core 1.1

---
+++ tests/apps/test-1.1/Controllers/TestsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test_1_1.Controllers
+{
+    /// <summary>
+    /// This controller implements all of the tests for this app.
+    /// </summary>
+    [Route("/")]
+    public class TestsController : Controller
+    {
+        /// <summary>
+        /// Basic serving test, verifies that the app is running by returning a well known
+        /// string.
+        /// </summary>
+        [HttpGet]
+        public string ServingTest()
+        {
+            return "Hello World!";
+        }
+    }
+}
