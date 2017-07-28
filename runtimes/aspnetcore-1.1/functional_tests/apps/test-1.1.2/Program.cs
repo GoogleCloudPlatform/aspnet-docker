2f2dc32a069642446903cd5acb4fc7eb65f279ad
Ivan Naranjo <ivann@google.com>
2017-07-28 13:35:22 +0100

    Adding the tests for the 1.1 runtime.

---
+++ runtimes/aspnetcore-1.1/functional_tests/apps/test-1.1.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_1._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
