a28f18a8a84f1d587d07264a1c704d58bb7ab2d3
Ivan Naranjo <ivannaranjo@users.noreply.github.com>
2017-08-16 11:53:49 +0100

    Adding the .NET Core 2.0 runtime. (#63)
    * Renaming to version 2.0.0.
    
    * Updating definition of the image's runtime and structural tests.
    
    * Adding the test for .NET core 2.0
    
    * Adding the published test app.
    
    * Adding functional tests for the .NET Core 2.0 runtime.
    
    * Adding entry for 2.0.0 runtime.
    
    * Adding integration test for 2.0.
    
    * Updating the integration tests.
    
    * Check the output of the functional test.
    
    * Verify the output of the functional tests.
    
    * Verify the output of the 1.1 tests.
    
    * Adding script to build staging images.
    
    * Adding the config files for the test.
    
    * Disable the manifest for the test.
    
    * Publish the entire app for testing.
    
    * Using the SDK image to provide the runtime store.
    
    * Create README.md
    
    * Update README.md

---
+++ runtimes/aspnetcore-2.0/functional_tests/apps/test-2.0.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_2._0._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
