f37567e6916e094e410f2b7945480ade5f5a5337
Ivan Naranjo <ivann@google.com>
2017-09-06 11:35:20 +0100

    Remove extra char.

--- tools/test.sh
+++ tools/test.sh
@@ -63,7 +63,7 @@ if [ -f $1/run_tests.yaml ]; then
 else
     readonly run_script=${workspace}/integration_tests/run_tests.yaml
 fi
-7
+
 # Choose the right .yaml file depending on whether we're deploying against the
 # canary or not.
 if [[ "${USE_FLEX_CANARY:-}" == "1" ]]; then
