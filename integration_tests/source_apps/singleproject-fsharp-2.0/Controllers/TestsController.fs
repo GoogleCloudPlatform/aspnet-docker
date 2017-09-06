942a0dfc41f1470f7b1b60b56b52b623c1d6b498
Ivan Naranjo <ivann@google.com>
2017-09-06 13:36:36 +0100

    Adding fsharp variant of integration test.

---
+++ integration_tests/source_apps/singleproject-fsharp-2.0/Controllers/TestsController.fs
@@ -0,0 +1,17 @@
+﻿namespace singleproject_fsharp_2._0.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+
+[<Route("/")>]
+type TestsController () =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Get() =
+        "Hello World!"
+
+
