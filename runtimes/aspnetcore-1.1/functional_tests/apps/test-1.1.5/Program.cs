244c6116b25b294247686dfdc70b42edf95de3bf
Ivan Naranjo <ivann@google.com>
2017-11-22 14:10:09 +0000

    Initial drop of the 1.1.5 runtime.

--- runtimes/aspnetcore-1.1/functional_tests/apps/test-1.1.5/Program.cs
+++ runtimes/aspnetcore-1.1/functional_tests/apps/test-1.1.5/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace test_1._1._4
+namespace test_1._1._5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World 1.1.4!");
+            Console.WriteLine("Hello World 1.1.5!");
         }
     }
 }
