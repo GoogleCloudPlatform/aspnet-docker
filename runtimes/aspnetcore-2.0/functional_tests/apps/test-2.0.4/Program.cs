af4add75c851012a6ffc984ee3dc5cf6c8c3dde7
Ivan Naranjo <ivannaranjo@users.noreply.github.com>
2018-01-10 11:34:00 +0000

    Adding the 2.0.4 runtime. (#81)
    * Adding the 2.0.4 runtime.
    
    * Updating to the 2.0.4 image.
    
    * Updated integration tests.

---
+++ runtimes/aspnetcore-2.0/functional_tests/apps/test-2.0.4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_2._0._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 2.0.4!");
+        }
+    }
+}
