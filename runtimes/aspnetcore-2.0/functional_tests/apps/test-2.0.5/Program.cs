56c2c9e7934282a45f8f83852349918886f4b572
Ivan Naranjo <ivannaranjo@users.noreply.github.com>
2018-01-31 18:17:16 +0000

    Adding new runtimes, 1.0.9, 1.1.6 and 2.0.5 (#85)
    * Initial drop of new runtime.
    
    * Initial drop of functional tests.
    
    * Adding the 1.1.6 runtime.
    
    * Adding the 1.1.6 runtime.
    
    * Adding 2.0.5 runtime.
    
    * Updating to the latest runtimes.
    
    * Update the integration tests.
    
    * Updating the integration tests.

---
+++ runtimes/aspnetcore-2.0/functional_tests/apps/test-2.0.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_2._0._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 2.0.5!");
+        }
+    }
+}
