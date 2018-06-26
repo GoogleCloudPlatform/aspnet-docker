c294a803487d39302b341b5b11a25ee217b255ae
Ian <iantalarico@users.noreply.github.com>
2018-06-26 10:51:38 -0700

    Upgrade from 2.1.0 to 2.1.1 (#98)

--- tools/build_runtimes.sh
+++ tools/build_runtimes.sh
@@ -45,7 +45,7 @@ gcloud container images add-tag ${repo}/aspnetcore:2.1-${TAG} ${repo}/aspnetcore
 gcloud container images add-tag ${repo}/aspnetcore:1.0-${TAG} ${repo}/aspnetcore:1.0.11 --quiet
 gcloud container images add-tag ${repo}/aspnetcore:1.1-${TAG} ${repo}/aspnetcore:1.1.8 --quiet
 gcloud container images add-tag ${repo}/aspnetcore:2.0-${TAG} ${repo}/aspnetcore:2.0.7 --quiet
-gcloud container images add-tag ${repo}/aspnetcore:2.1-${TAG} ${repo}/aspnetcore:2.1.0 --quiet
+gcloud container images add-tag ${repo}/aspnetcore:2.1-${TAG} ${repo}/aspnetcore:2.1.1 --quiet
 
 # Tag latest.
 gcloud container images add-tag ${repo}/aspnetcore:2.1-${TAG} ${repo}/aspnetcore:latest --quiet
