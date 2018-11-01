7101a013dc54bcfe75e8f79a073e830b76232eaf
sharifelgamal <selgamal@google.com>
2018-11-01 13:43:49 -0700

    Update aspnetcore 1.x versions to 1.0.13 and 1.1.10

--- tools/build_runtimes.sh
+++ tools/build_runtimes.sh
@@ -42,8 +42,8 @@ gcloud container images add-tag ${repo}/aspnetcore:2.0-${TAG} ${repo}/aspnetcore
 gcloud container images add-tag ${repo}/aspnetcore:2.1-${TAG} ${repo}/aspnetcore:2.1 --quiet
 
 # Tag minor versions.
-gcloud container images add-tag ${repo}/aspnetcore:1.0-${TAG} ${repo}/aspnetcore:1.0.12 --quiet
-gcloud container images add-tag ${repo}/aspnetcore:1.1-${TAG} ${repo}/aspnetcore:1.1.9 --quiet
+gcloud container images add-tag ${repo}/aspnetcore:1.0-${TAG} ${repo}/aspnetcore:1.0.13 --quiet
+gcloud container images add-tag ${repo}/aspnetcore:1.1-${TAG} ${repo}/aspnetcore:1.1.10 --quiet
 gcloud container images add-tag ${repo}/aspnetcore:2.0-${TAG} ${repo}/aspnetcore:2.0.9 --quiet
 gcloud container images add-tag ${repo}/aspnetcore:2.1-${TAG} ${repo}/aspnetcore:2.1.5 --quiet
 
